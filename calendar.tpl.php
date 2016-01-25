68f8d9d49bc28b4840aad67a577d3011098e7a90
Dan Rogers <dan.rogers@idfive.com>
2016-01-25 16:40:50 -0500

    rename unical_client

--- calendar.tpl.php
+++ calendar.tpl.php
@@ -2,6 +2,6 @@
   <div ng-view></div>
 </div>
 <script>
-  window.siteId = Drupal.settings.unical_embed_variables.siteId;
-  window.siteUrl = Drupal.settings.unical_embed_variables.siteUrl + 'api';
+  window.siteId = Drupal.settings.unical_client_variables.siteId;
+  window.siteUrl = Drupal.settings.unical_client_variables.siteUrl + 'api';
 </script>
